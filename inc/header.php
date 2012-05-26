6588a10f815ebdf3694ebb6527ce9d743d0844d8
AshleyBalentine <ashley@btw-services.com>
2012-05-25 21:41:52 -0500

    added php files

---
+++ inc/header.php
@@ -0,0 +1,7 @@
+<div class="container">
+	<div id="header">
+	echo "James Scott Balentine";
+	echo "Composer - Performer- Music Educator";
+	id="border"
+	echo ""...his music is magical, and has a wry sense of humor...it lifts my spirit and awakens my heart."";
+	echo "img src="

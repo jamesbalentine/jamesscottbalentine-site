285ab40520ca73aa7a83056d20e7124f7d4ac8b6
AshleyBalentine <ashley@btw-services.com>
2012-05-25 21:42:26 -0500

    added php file

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<?php include 'inc/scripts.php'; ?>
+	</head>
+	<body>
+		<?php include 'inc/header.php'; ?>
+		<div class='content'; ?>
+		<?php include 'biography'; ?>
+		<?php include 'catalog'; ?>
+		<?php include 'news'; ?>	
+		</div>
+		<?php inc 'inc/footer.php'; ?>
+	</body>
+</html>

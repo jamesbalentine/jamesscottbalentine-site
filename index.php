6f3030813dc80060898410fea3c8353bec64c8bc
BenBalentine <myrcutio@gmail.com>
2012-05-25 22:12:14 -0500

    added body logic to index

--- index.php
+++ index.php
@@ -5,9 +5,24 @@
 	<body>
 		<?php include 'inc/header.php'; ?>
 		<div class='content'; ?>
-			<?php 	include 'biography.html'; 
-					include 'news.html'; 
-			?>	
+			<?php 
+				$page = filter_var($_GET["page"], FILTER_SANITIZE_NUMBER_INT);
+
+				switch($page)
+				{
+				    case '1';
+				        include 'homepage.html';
+				    break;
+				    case '2';
+				        include 'news.html'; 
+				    break;
+				    case '3';
+				        include 'biography.html'; 
+				    break;
+				    default;
+				    break;
+				}
+			?>
 		</div>
 		<?php inc 'inc/footer.php'; ?>
 	</body>

61e7361f50690978c2e1c8cfdfb074d214ba56bf
AshleyBalentine <ashley@btw-services.com>
2012-05-27 12:25:26 -0500

    Homepage with images.

---
+++ inc/homepage.php
@@ -0,0 +1,11 @@
+<div class="container">
+<div id="header-quote">"...his music is magical, and has a wry sense of humor...it lifts my spirit and awakens my heart."</div>
+<div style="width: 256px; height: 304px; text-align: center; vertical-align: top; background:#000 url('Images/JSB%20Gazebo%20Soft.png') no-repeat right top; background-size:256px 304px;" alt="JSB Gazebo"></div>
+<div>
+          <img style="width: 402px; height: 296px; margin-top: 75px;" 
+                src="Images/Navarro%20Soft.png" alt="Navarro Rehearsal">
+    </div>
+          <span class="caption2">conducting a rehearsal of Navarro, a children's opera
+          </span>
+    </div>
+</div>

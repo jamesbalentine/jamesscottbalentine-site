9b372e902d427cba86f50b19a66710cf5987121c
AshleyBalentine <ashley@btw-services.com>
2012-05-27 11:06:52 -0500

    Format change

---
+++ inc/biography.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html><head>
+      <meta content="text/html; charset=ISO-8859-1" http-equiv="content-type">
+      <title>JSB Biography</title>
+      <link rel="stylesheet" type="text/css" href="css/jsbstyles.css" />
+</head><body>
+
+ <?php 
+
+ include "inc/header.php";
+
+?>
+
+    <table style="text-align: center; margin-left: auto; margin-right: auto; height: auto; width: 782px; background-color: black;"        cellpadding="10" cellspacing="2">
+        <tr>
+          <td colspan="3" style="text-align: center; margin-top: 10px; font-family: Palatino; font-size: 14px;">
+                    Composer by character and performer by temperament, James Scott Balentine (Jim to those who know him) is about as complex as his music; that is, moderately mysterious, yet engaging. Jim&#8217;s
+                    music is fun and interesting to play, engaging to the listener, and crafted in a personal language influenced by ethnic dance, jazz, folk idioms, and atonal and serial techniques when they seem
+                    appropriate.
+          </td>
+        </tr>
+         <tr>
+            <td style="vertical-align: top;">
+                <img width="300px"; height="400px" alt="McNay" src="Images/jsbedinburghsoft.png">
+            </td>
+
+      
+      <td style="text-align: justify; vertical-align: top; font-size: 14px; colspan="1" rowspan="3"">
+                
+      
+      <span style="color: white; font-family: Palatino;">
+                  <center>His music has been commissioned by soloists and ensembles including:</center>
+                <ul>
+                    <li> The Barlow Endowment
+                    <li> The Opera Guild of San Antonio
+                    <li> The Texas Clarinet Consort
+                    <li> College Band Directors National Association
+                    <li> Robert Walzel, clarinet
+                    <li> Timothy Jones, baritone
+                    <li> Rita Linard, Flute
+                    <li> Cactus Pear Music Festival
+                    <li> The Krost Symposium
+                    <li> The City of San Antonio Arts and Cultural Affairs
+                </ul>
+                  <center>Awards, Grants and honors from:</center>
+                <ul>
+                    <li> The American Music Center
+                    <li> The Artist Foundation of San Antonio
+                    <li> Conservatory aan Zee, Ostend, Belgium
+                    <li> ASCAP
+                </ul>
+                  <center>With performances at:</center>
+                <ul>
+                   <li> Utah Arts Festival
+                   <li> International Clarinet Association ClarinetFest
+                   <li> Indiana State University New Music Festival
+                   <li> Shanghai International Clarinet Festival
+                   <li> Southwest Guitar Festival, and many more
+                </ul>
+                
+                  A prolific arranger as well as composer, Jim&#8217;s arrangements have been performed by the San Antonio
+                  Symphony, Phoenix Symphony, and others, and his big band charts are included in the libraries of big bands across the United States.<br><br>
+
+                  A professional performer since high school, Jim is a freelance musician, primarily as a woodwind performer (clarinets, saxophones and flutes) in big bands, pit orchestras, and an eclectic mix of orchestra, bands and ensembles that includes performances with the San Antonio Symphony, the Houston Jazz Orchestra, and backing bands for Ray Charles, Mel Torme, Maureen McGovern, Bob Hope, Tom Jones, John Denver, Johnny Mathis and many, many others. 
+                  <br><br>
+                  Professor of Music Theory and Composition at the University of Texas at San Antonio since 1989, Jim teaches
+                  courses in music composition, theory, and jazz studies. He received bachelor&#8217;s and master&#8217;s degrees from the University of South Carolina and the Doctor of Musical Arts in Composition from the University of Texas at Austin. His composition teachers included Hunter Johnson, Gordon (Dick) Goodwin, Samuel Douglas, Eugene Kurtz, Barton McLean and Karl Korte. He has also served as Director of Jazz Studies at Sam Houston State University in Huntsville, Texas, and was founder and owner of One Music Square, a full line retail music store.</span>
+              <br><br>
+
+      <center>Personal</center><br>
+      
+                  Born in Fort Worth, Texas in 1947, the son of a career Air Force officer, Jim was raised in Paris, France;
+                  Montgomery, Alabama; Tachikawa, Japan, and has since lived (from as little as six weeks to ten years) in such diverse locations as Winnipeg (Canada), Edinburgh (Scotland), and Zurich (Switzerland); Midwest City
+                  (Oklahoma), San Francisco, Houston, Austin, and Huntsville (Texas), and currently resides in the hill country near San Antonio. He is a certificated private pilot, a pretty good cook, and a passable tennis player (pun
+                  intended). Jim and his wife Cindy have three grown children, James, David and Ben, a granddaughter, Terra, and
+                  have inherited three beautiful and amazing daughters-in-law, Angie, Elizabeth, and Ashley, from the families McBride, Pena, and McGee, for whom we are exceedingly grateful. <center>We Balentines have been lucky indeed!</center></span>
+      </td>
+    </tr>
+</table>
+<?php
+
+ include "inc/footer.php";
+
+ ?>
+ 
+</body>
+</html>

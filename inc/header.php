06f046c55cfc9c288719a2836581ae5286f03391
AshleyBalentine <ashley@btw-services.com>
2012-05-27 12:28:31 -0500

    Quote removed.

--- inc/header.php
+++ inc/header.php
@@ -1,5 +1,4 @@
 <div id="header" class="container">
 	<div id="header-text">James Scott Balentine</div>
 	<div id="header-subtext">Composer - Performer- Music Educator</div>
-	<div id="header-quote">"...his music is magical, and has a wry sense of humor...it lifts my spirit and awakens my heart."</div>
 </div>

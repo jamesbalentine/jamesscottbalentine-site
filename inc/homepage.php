a54ddf9f18e20e34663e6881134fda2909e4b622
BenBalentine <myrcutio@gmail.com>
2012-05-29 19:30:55 -0500

    updated links on homepage

--- inc/homepage.php
+++ inc/homepage.php
@@ -2,11 +2,11 @@
 <div id="header-quote">"...his music is magical, and has a wry sense of humor...it lifts my spirit and awakens my heart."</div>
 <div style="width: 256px; height: 304px; text-align: center; vertical-align: top; background:#000 url('Images/JSB%20Gazebo%20Soft.png') no-repeat right top; background-size:256px 304px;" alt="JSB Gazebo"></div>
 <div class="Links">
-	<a href="JSBbiography.html">BIOGRAPHY</a>
-  <a href="JSBCatalog.html">CATALOG OF MUSIC</a>
+  <a href="index.php?page=1">CATALOG OF MUSIC</a>
+  <a href="index.php?page=2">NEWS AND EVENTS</a>
+	<a href="index.php?page=3">BIOGRAPHY</a>  
   <a href="Scores.html">FULL SCORES IN pdf</a>
   <a href="Listen.html">LISTEN</a>
-  <a href="News.html">NEWS AND EVENTS</a>
   <a href="Links.html">LINKS</a>
   <a href="Contact.html">CONTACT ME</a>
 </div>

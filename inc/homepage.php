86d6b0d92a5f8e481fc00a785cbf7f9bb57af970
AshleyBalentine <ashley@btw-services.com>
2012-05-29 20:10:31 -0500

    Wraps quote and image with styling.

--- inc/homepage.php
+++ inc/homepage.php
@@ -1,6 +1,8 @@
 <div class="container">
+<div id="image-quote-wrapper">
 <div style="width: 256px; height: 304px; text-align: center; vertical-align: top; background:#000 url('Images/JSB%20Gazebo%20Soft.png') no-repeat right top; background-size:256px 304px;" alt="JSB Gazebo"></div>
 <div id="header-quote">"...his music is magical, and has a wry sense of humor...it lifts my spirit and awakens my heart."</div>
+</div>
 <div class="Links">
   <a href="index.php?page=1">CATALOG OF MUSIC</a>
   <a href="index.php?page=2">NEWS AND EVENTS</a>

fceaa67978ddc9b39f2f764f31242fa5e7b9ae18
BenBalentine <myrcutio@gmail.com>
2012-05-29 19:22:01 -0500

    spacing

--- inc/homepage.php
+++ inc/homepage.php
@@ -2,13 +2,13 @@
 <div id="header-quote">"...his music is magical, and has a wry sense of humor...it lifts my spirit and awakens my heart."</div>
 <div style="width: 256px; height: 304px; text-align: center; vertical-align: top; background:#000 url('Images/JSB%20Gazebo%20Soft.png') no-repeat right top; background-size:256px 304px;" alt="JSB Gazebo"></div>
 <div class="Links">
-	<a href="JSBbiography.html">BIOGRAPHY</a><br>
-    <a href="JSBCatalog.html">CATALOG OF MUSIC</a><br>
-    <a href="Scores.html">FULL SCORES IN pdf</a><br>
-    <a href="Listen.html">LISTEN</a><br>
-    <a href="News.html">NEWS AND EVENTS</a><br>
-    <a href="Links.html">LINKS</a><br>
-    <a href="Contact.html">CONTACT ME</a>
+	<a href="JSBbiography.html">BIOGRAPHY</a>
+  <a href="JSBCatalog.html">CATALOG OF MUSIC</a>
+  <a href="Scores.html">FULL SCORES IN pdf</a>
+  <a href="Listen.html">LISTEN</a>
+  <a href="News.html">NEWS AND EVENTS</a>
+  <a href="Links.html">LINKS</a>
+  <a href="Contact.html">CONTACT ME</a>
 </div>
 <div>
           <img style="width: 402px; height: 296px; margin-top: 75px;" 

dd3e1488d3b6a715cad0835002179352429dddf6
AshleyBalentine <ashley@btw-services.com>
2012-06-05 09:41:40 -0500

    Edits made to homepage.php text. div=class "image-quote-credit" styled.

--- inc/homepage.php
+++ inc/homepage.php
@@ -2,7 +2,7 @@
   <div id="header-quote">"...his music is magical, and has a wry sense of humor...it lifts my spirit and awakens my heart."--Tina Lear--Lyricist, Composer, Poet, Songwriter</div>
   <div id="image-quote-wrapper">
     <div style="width: 256px; height: 304px; text-align: center; vertical-align: top; background:#000 url('Images/JSB%20Gazebo%20Soft.png') no-repeat right top; background-size:256px 304px;" alt="JSB Gazebo"></div>
-    <div class="image-quote-credit">photo by Susan Riley </div>
+    <div class="image-quote-credit">Photo by Susan Riley </div>
   </div>
   <div class="Links">
     <a href="index.php?page=1">CATALOG OF MUSIC</a>
@@ -16,5 +16,5 @@
   <div>
     <img style="width: 402px; height: 296px; margin-top: 75px;" src="Images/Navarro%20Soft.png" alt="Navarro Rehearsal">
   </div>
-  <span class="caption2">conducting a rehearsal of Navarro, a children's opera</span>
+  <span class="caption2">Conducting a rehearsal of Navarro, a children's opera</span>
 </div>

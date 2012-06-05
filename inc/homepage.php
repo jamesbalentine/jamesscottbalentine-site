ebc645fbb9732bd5c8d7d73e8ec12ef6672a1fd1
AshleyBalentine <ashley@btw-services.com>
2012-06-05 09:33:16 -0500

    Text changes made to quote.

--- inc/homepage.php
+++ inc/homepage.php
@@ -1,5 +1,5 @@
 <div class="container">
-  <div id="header-quote">"...his music is magical, and has a wry sense of humor...it lifts my spirit and awakens my heart."--Tina Lear</div>
+  <div id="header-quote">"...his music is magical, and has a wry sense of humor...it lifts my spirit and awakens my heart."--Tina Lear--Lyricist, Composer, Poet, Songwriter</div>
   <div id="image-quote-wrapper">
     <div style="width: 256px; height: 304px; text-align: center; vertical-align: top; background:#000 url('Images/JSB%20Gazebo%20Soft.png') no-repeat right top; background-size:256px 304px;" alt="JSB Gazebo"></div>
     <div class="image-quote-credit">photo by Susan Riley </div>

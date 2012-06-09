c401125ccf4759008081dde77aef099c4a13f3fa
BenBalentine <myrcutio@gmail.com>
2012-06-09 12:55:34 -0500

    styles and fonts

--- inc/scripts.php
+++ inc/scripts.php
@@ -1,5 +1,1 @@
-<style type="application/x-font-TrueType">
-	@font-face {font-family: "Palatino"; src: url(../fonts/palatino.ttf) format("truetype");}
-	body {font-family: "Palantino";}
-</style>
 <link rel="stylesheet" type="text/css" href="css/jsbstyles.css">

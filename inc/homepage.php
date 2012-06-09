03a066018c40684c54ae558b580e39ed1615556a
AshleyBalentine <ashley@btw-services.com>
2012-06-09 13:15:11 -0500

    styles and a wrapper

--- inc/homepage.php
+++ inc/homepage.php
@@ -1,19 +1,21 @@
-<div id="main-content" class="container">
-  <div id="header-quote">"...his music is magical, and has a wry sense of humor...it lifts my spirit and awakens my heart."--Tina Lear--Lyricist, Composer, Poet, Songwriter</div>
-  <div id="image-quote-wrapper">
-    <div style="width: 256px; height: 304px; text-align: center; vertical-align: top; background:#000 url('Images/JSB%20Gazebo%20Soft.png') no-repeat right top; background-size:256px 304px;" alt="JSB Gazebo"></div>
-    <div class="image-quote-credit">Photo by Susan Riley </div>
+<div id="main-content-wrapper">
+  <div id="main-content" class="container">
+    <div id="header-quote">"...his music is magical, and has a wry sense of humor...it lifts my spirit and awakens my heart."--Tina Lear--Lyricist, Composer, Poet, Songwriter</div>
+    <div id="image-quote-wrapper">
+      <div style="width: 256px; height: 304px; text-align: center; vertical-align: top; background:#000 url('Images/JSB%20Gazebo%20Soft.png') no-repeat right top; background-size:256px 304px;" alt="JSB Gazebo"></div>
+      <div class="image-quote-credit">Photo by Susan Riley </div>
+    </div>
+    <div class="Links">
+      <a href="index.php?page=1">CATALOG OF MUSIC</a>
+      <a href="index.php?page=2">NEWS AND EVENTS</a>
+    	<a href="index.php?page=3">BIOGRAPHY</a>  
+      <a href="Scores.html">FULL SCORES IN pdf</a>
+      <a href="Listen.html">LISTEN</a>
+      <a href="Links.html">LINKS</a>
+      <a href="Contact.html">CONTACT ME</a>
+    </div>
+    <div class="image-captioned">
+      <img style="width: 402px; height: 296px; margin-top: 75px;" src="Images/Navarro%20Soft.png" alt="Navarro Rehearsal">
+    <div class="caption2">Conducting a rehearsal of Navarro, a children's opera</div>
   </div>
-  <div class="Links">
-    <a href="index.php?page=1">CATALOG OF MUSIC</a>
-    <a href="index.php?page=2">NEWS AND EVENTS</a>
-  	<a href="index.php?page=3">BIOGRAPHY</a>  
-    <a href="Scores.html">FULL SCORES IN pdf</a>
-    <a href="Listen.html">LISTEN</a>
-    <a href="Links.html">LINKS</a>
-    <a href="Contact.html">CONTACT ME</a>
-  </div>
-  <div class="image-captioned">
-    <img style="width: 402px; height: 296px; margin-top: 75px;" src="Images/Navarro%20Soft.png" alt="Navarro Rehearsal">
-  <div class="caption2">Conducting a rehearsal of Navarro, a children's opera</div>
 </div>

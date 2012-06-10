21f16268b1b827307084bfd8f14cec5f16dfdb41
AshleyBalentine <ashley@btw-services.com>
2012-06-10 18:05:23 -0500

    Fuck fuck fuckity fuck myself.

--- inc/contact.php
+++ inc/contact.php
@@ -1,6 +1,7 @@
 <div id="main-content" class="container">
 	<div class="body">
-		<div id="books-image" img src="Images/books.png">
+		<div id="books-image"> 
+		<img src="Images/books.png">
 		</div>
 	</div>
 </div>

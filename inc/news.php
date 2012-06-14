3e9cf27f376f10c02ef6bf490c3813a07fd47620
AshleyBalentine <ashley@btw-services.com>
2012-06-14 15:35:17 -0500

    Changed image type. Also am checking image locations.

--- inc/news.php
+++ inc/news.php
@@ -1,6 +1,6 @@
 <div id="main-content" class="container">
 	<div class="main-content-body">
-		<div id="mcnayimage"><img src="../Images/mcnay2.jpg"></div>
+		<div id="mcnayimage"><img src="../Images/mcnaysoft.png"></div>
 		<div id="newscaption">Outside the McNay Art Museum, San Antonio</div>
 	</div>
 </div>

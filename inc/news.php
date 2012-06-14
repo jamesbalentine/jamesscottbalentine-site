6d681bcbb59cc9cff12f633e0ba881fe07fa3c8d
AshleyBalentine <ashley@btw-services.com>
2012-06-14 15:32:20 -0500

    Image accidentally in two places, which is not a huge deal, but not necessary.

--- inc/news.php
+++ inc/news.php
@@ -1,6 +1,7 @@
 <div id="main-content" class="container">
+	<div class="main-content-body">
 		<div id="mcnayimage"><img src="../Images/mcnay2.jpg"></div>
 		<div id="newscaption">Outside the McNay Art Museum, San Antonio</div>
-		<div id="booksimage"><img src="../Images/books.png"></div>
+	</div>
 </div>
 

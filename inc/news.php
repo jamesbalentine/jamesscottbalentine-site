c00bcd60401695fe2517374a71f951d3dd7d23db
AshleyBalentine <ashley@btw-services.com>
2012-06-14 15:29:20 -0500

    Cleaning up code.

--- inc/news.php
+++ inc/news.php
@@ -1,8 +1,6 @@
 <div id="main-content" class="container">
-	<div class="container">
-			<div id="mcnayimage"><img src="../Images/mcnay2.jpg"></div>
-			<div id="newscaption">Outside the McNay Art Museum, San Antonio</div>
-			<div id="booksimage"><img src="../Images/books.png"></div>
-
-	</div>
+		<div id="mcnayimage"><img src="../Images/mcnay2.jpg"></div>
+		<div id="newscaption">Outside the McNay Art Museum, San Antonio</div>
+		<div id="booksimage"><img src="../Images/books.png"></div>
 </div>
+

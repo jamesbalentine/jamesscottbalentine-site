e1130f09cd19fdd824ec7dc846299e2fe7e92231
AshleyBalentine <ashley@btw-services.com>
2012-06-19 10:37:13 -0500

    Made class tag in css for li style on contact.php; fixed image on biography.php; and did some other stuff that hadn't gotten changed.

--- inc/biography.php
+++ inc/biography.php
@@ -4,8 +4,7 @@
     music is fun and interesting to play, engaging to the listener, and crafted in a personal language influenced by ethnic dance, jazz, folk idioms, and atonal and serial techniques when they seem
     appropriate.
   </div>
-  <div id="mcnayimage"><img src="Images/jsbedinburghsoft.png">
-  </div>
+  <div id="mcnayimage"><img src="Images/jsbedinburghsoft.png"></div>
   <div class="paragraph-commissions">His music has been commissioned by soloists and ensembles including:</div>
     <ul>
       <li> The Barlow Endowment

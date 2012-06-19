3e61eb81d21e4f28623870202f06405091093852
AshleyBalentine <ashley@btw-services.com>
2012-06-19 10:49:12 -0500

    Lets see if this doesn't work. Removed the list items and made them paragraphs.

--- inc/contact.php
+++ inc/contact.php
@@ -1,21 +1,23 @@
 <div id="main-content" class="container">
 	<div id="main-content-body">
 		<div id="books-image"><img src="Images/books.png"></div>
-		<li>You may download pdf scores from this web site to print, study, or help prepare music for
+		<div class="paragraph">
+		<p>You may download pdf scores from this web site to print, study, or help prepare music for
 		performance. By downloading any music from this web site, you agree that you will not sell it in
-		any form, printed or electronic.</li>
-		<li>If you would like instrumental parts for performance, please send your request to <a
+		any form, printed or electronic.</p>
+		<p>If you would like instrumental parts for performance, please send your request to <a
 		href="mailto:contact@guildhian.com?subject=Music PDF Request">Guildhian Music</a>,
 		including information on the performers and intended date(s) of performance, and in most
 		cases, these will be sent via email as pdf documents at no charge. You agree that you may not
-		sell these parts in any form.</li>
-		<li>If you would like to have professionally printed scores and/or parts, send an email to <a
+		sell these parts in any form.</p>
+		<p>If you would like to have professionally printed scores and/or parts, send an email to <a
 		href="mailto:contact@guildhian.com?subject=Music Order Request">Guildhian Music</a>
 		with information on the music you wish to purchase, and it will be shipped to you with an
 		invoice which will be payable upon receipt. Prices for each piece are listed below, and actual
-		shipping costs will be added to the invoice.</li>
-		<li>Music Dealers – <a href="mailto:contact@guildhian.com?subject=Dealer Info
+		shipping costs will be added to the invoice.</p>
+		<p>Music Dealers – <a href="mailto:contact@guildhian.com?subject=Dealer Info
 		Request">Guildhian Music</a> offers wholesale terms to music dealers for our print music.
-		Terms vary according to quantity per item and payment due date. Please inquire via email.</li>
+		Terms vary according to quantity per item and payment due date. Please inquire via email.</p>
+	</div>
     </div>
 </div>

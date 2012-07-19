9c879aad522bd91b7bfd0d983e54c600423c3c7f
BenBalentine <myrcutio@gmail.com>
2012-07-19 01:30:29 -0500

    fixed audio player

--- inc/scripts.php
+++ inc/scripts.php
@@ -1,7 +1,4 @@
 <link rel="stylesheet" type="text/css" href="http://guildhian.com/staging/css/jsbstyles.css">
-<link rel="stylesheet" type="text/css" href="http://jquerytools.org/media/css/scrollable-horizontal.css">
-<link rel="stylesheet" type="text/css" href="http://jquerytools.org/media/css/scrollable-buttons.css">
-<script src="http://cdn.jquerytools.org/1.2.7/full/jquery.tools.min.js"></script>
 <script type="text/javascript">
 	function loadBody(page){
 		$.ajax({
--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 ?>
 <html>
 	<head>
-		<script src="css/player.css"></script>
+		<link rel="stylesheet" type="text/css" href="css/player.css">
 	</head>
 	<body>
 		<div style="position: fixed; bottom: 0; left: 200px; z-index: 1;">
-			<?php include "inc/player.php"; ?>
+			<?php include "inc/audioplayer.html"; ?>
 		</div>
 		<iframe id="inner" name="inner" src="inc/innerframe.php" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></iframe>
 	</body>

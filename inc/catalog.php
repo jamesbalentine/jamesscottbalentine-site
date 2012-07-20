7ea460c689740f0f4649ea654bba0b064fccff32
AshleyBalentine <ashley@btw-services.com>
2012-07-19 20:56:43 -0500

    Additions to the catalog. More stuff coming.

--- inc/catalog.php
+++ inc/catalog.php
@@ -4,11 +4,55 @@
     <div id="piano-image"><img src="../Images/PianoMusicSoft.png"></div>
     <div id="image-logo"><img src="../Images/GuildhianLogoInvert.gif"></div>  
   </div>
-</div>
 <div class="container">
-  <div class="catalog-category"><i>Orchestra</i></div>
-       <div id="catalog-title">"D&ugrave;n &Egrave;ideann Blogh"</div>
+  <div id="catalog-category"><i>Orchestra</i>
+       <div id="catalog-title">"D&ugrave;n &Egrave;ideann Blogh"></div>
           <div id="title-description">Double concerto for clarinet, bassoon and chamber orchestra</div>
        <div id="catalog-title">"Triqueta"</div>
-           <div id="title-description">Double concerto for horn, guitar, piano, percussion, and strings</div>       
+           <div id="title-description">Double concerto for horn, guitar, piano, percussion, and strings</div>
+       <div id="catalog-title">"In Just Spring"></div> 
+           <div id="title-description">Song cycle for soprano voice, piano, and string orchestra</div>
+       <div id="catalog-title">"A Glass Box"</div>
+            <div id="title-description">Song cyle for soprano, alto, and baritone voices and chamber orchestra</div>
+       <div id="catalog-title">"Nalost"</div>
+            <div id="title-description">For orchestra</div>
+  </div>
+  <div class="catalog-category"><i>Wind Ensemble</i>
+      <div id="catalog-title">"Red Poppy and the Yellow Rose"></div>
+            <div id="title-description">Commissioned by The University of Texas at San Antonio for the Silver Anniversary Convocation, September, 1994. Duration 4 minutes</div>
+      <div id="catalog-title">"The Image of Dreams"</div>
+            <div id="title-description">Ensemble commissioned by Robert Walzel. Duration: 6 minutes</div>
+      <div id="catalog-title">"The White Goddess" (1994)</div>
+            <div id="title-description">Solo Clarinet and Wind Ensemble. Written for Ron Wilkins and the UTSA Wind Ensemble. Duration: 12 minutes></div>
+      <div id="catalog-title">"Pagan from the Natural Light" (1992)</div>
+            <div id="title-description">Solo Trombone and Wind Ensemble. Commissioned by The College Band Directors National Association; performed by California State University, Long Beach Wind Ensemble at CBDNA Convention, Atlanta 1982. Duration: 12 minutes</div>
+      <div id="catalog-title">"Goodnight to the Old Gods" (1982)</div>
+            <div id="catalog-description">Wind Ensemble</div>
+  </div>
+  <div class="catelog-category"><i>Brass Ensemble></i>
+      <div id="catalog-title">"Satchmo" (2000)</div>
+            <div id="title-description">For large brass ensemble and percussion. Commissioned for UTSA Commencement, Spring 1997. Also available as brass quintent. Duration: 3 minutes</div>
+      <div id="catalog-title">Fanfare and Fugue (1991)</div>
+            <div id="title-description">Written for UTSA Commencement exercises. Duration: 2 minutes</div>
+  <div id="catalog-category"><i>Choral</i>
+      <div id="catalog-title">"The Present" (2004)</div>
+            <div id="title-description">For children's chorus, adult choir and piano. Text by Tina Lear. Commissioned by the Children's Chorus of San Antonio for their 20th anniversary. Duration: 5 minutes</div>
+  </div>
+  <div class="catalog-category"><i>Opera</i>
+      <div id="catalog-title">"Navarro: A Children's Opera" (2011)</div>
+            <div id="title-description">A children's opera for 12 singers and chamber orchestra; libretto by Joe Labatt. Commissioned by the Opera Guild of San Antonio. Premiered by UTSA Lyrics Opera, September 24, 2011. Duration: 45 minutes</div>
+  </div>
+  <div class="catalog-category"><i>Chamber Music</i>
+      <div id="catalog-title">"What Water Says" (2012)</div>
+            <div id="title-description">For baritone voice and guitar. Commissioned by David Asbury and Bruce Cain for <i>River of Words</i> Project, Library of Congress. Duration: 2 minutes</div>
+      <div id="catalog-title">"Liberty Bar" (2010)</div>
+            <div id="title-description">For two percussionists: Marimba/percussion and Vibraphone/percussion. Commissioned by Sherry Rubins. Duration: 4 minutes, 30 seconds</div>
+      <div id="catalog-title">"Red Poppy and the Yellow Rose" (2009)</div>
+            <div id="title-description">Clarinet choir with double clarinet quartets. Commissioned by Guido Six, director of the Claribel Clarinet Choir, Ostend Conservatory of Music, Ostend, Belgium. Premiere at Texas Music Educators Association Convention, February, 2010. Duration: 6 minutes</div>
+      <div id="catalog-title">"The Ghost of Eric Dolphy" (2010)</div>
+            <div id="title-description">Clarinet sextet. Commissioned by the Texas Clarinet Consort. Premiered at the International Clarinet Association Convention (ClarinetFest), July 2010. Duration: 6 minutes</div>
+      <div id="catalog-category">"Monk, Through a Glass Darkly" (2002)</div>
+            <div id="title-description">Clarinet sextet. Commissioned by Texas Clarinet Consort for ClarinetFest 2002, Stockholm. 
+  </div>
+  </div>     
   </div>

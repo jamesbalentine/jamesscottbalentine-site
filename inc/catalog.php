bdcdfe716f47daaaa1d723b194a34faefeea6aaf
AshleyBalentine <ashley@btw-services.com>
2012-07-21 14:47:05 -0500

    Holy crap, that was boring.

--- inc/catalog.php
+++ inc/catalog.php
@@ -51,8 +51,76 @@
             <div id="title-description">Clarinet choir with double clarinet quartets. Commissioned by Guido Six, director of the Claribel Clarinet Choir, Ostend Conservatory of Music, Ostend, Belgium. Premiere at Texas Music Educators Association Convention, February, 2010. Duration: 6 minutes</div>
       <div id="catalog-title">"The Ghost of Eric Dolphy" (2010)</div>
             <div id="title-description">Clarinet sextet. Commissioned by the Texas Clarinet Consort. Premiered at the International Clarinet Association Convention (ClarinetFest), July 2010. Duration: 6 minutes</div>
-      <div id="catalog-category">"Monk, Through a Glass Darkly" (2002)</div>
+      <div id="catalog-title">"Monk, Through a Glass Darkly" (2002)</div>
             <div id="title-description">Clarinet sextet. Commissioned by Texas Clarinet Consort for ClarinetFest 2002, Stockholm. 
+      <div id="catalog-title">"A Toot in Ninesia" (1996)</div>
+            <div id="title-description">For clarinet sextet. Commissioned by the Texas Clarinet Consort for Clarinet Fest 1996, Paris, France. Duration: 6 minutes</div>
+      <div id="catalog-title">"3 Spirituals" (1999)</div>
+            <div id="catalog-description">For clarinet sextet. Commissioned by the Texas Clarinet Consort for ClarinetFest 2000, Ostend, Belgium. Duration: 5 minutes</div>
+      <div id="catalog-title">"Nine Wild Exotic Dances"</div>
+            <div id="title-description">For clarinet and piano. Commissioned by Robert Walzel for ClarinetFest 1999, Ostende, Belgium. Duration: 18 minutes</div>
+      <div id="catalog-title">"March, Strashspey and Reel" (2003)</div>
+            <div id="title-description">For clarinet and piano. Commissioned by The Barlow Endowment for ClarinetFest 2003, Salt Lake City, Utah. Premiered by Jaren Hinckley. Duration: 8 minutes</div>
+      <div id="catalog-title">"The Charlie Effect" 2004</div> 
+            <div id="title-description">For clarinet, bass clarinet and piano. Commissioned by Elizabeth Crawford Kilpatrick for ClarinetFest 2004 Washington D.C.. Duration: 5 minutes</div>
+      <div id="catalog-title">"Line Drawings" (1997)></div>
+            <div id="title-description">For Clarinet and marimba. Commissioned for the International Clarinet Society Convention.</div>
+      <div id="catalog-title">"In a Bright Room" (1991)</div>
+            <div id="title-description">For clarinet and bassoon. Commissioned by Robert Walzel, clarinet, and Scott Walzel, bassoon. Duration: 4 minutes. 
+      <div id="catalog-title">"Devils Advice to Story Tellers" (1986)</div>
+            <div id="title-description">Solo unaccompanied clarinet. Commissioned by Robert Walzel for doctoral recital, UNT. Duration: 8 minutes</div>
+      <div id="catalog-title">"The Graham Trio" (1978)</div>
+             <div id="title-description">For clarinet, cello and piano. Written for R. Douglas Graham, clarinet, University of South Carolina.</div>
+      <div id="catalog-title">"Piano Sonata No.1" (1995)</div>
+             <div id="title-description">Solo piano.
+      <div id="catalog-title">"Piano Sonata No.2" (2006)</div>
+            <div id="title-description">Solo piano. Written for Roger Steptoe. Premiered at American Landmark Festival Performances at Central Presbyterian Church, Manhattan, New York, and Smith Opera House, Geneva, New York March 2007.</div>
+      <div id="catalog-title">"A Trilogy for Friends" (2002)</div>
+            <div id="title-description">Solo piano. Written for Kasandra Keeling in memory of Kathy Hodges, Reed Holmes and J.K. Hodges. Duration: 8 minutes</div>
+      <div id="catalog-title">"Les Quatre Coins" (2005)</div>
+             <div id="title-description">For flute, oboe, cello and piano. Commissioned by the Hemisphere Quartet and Ensemble for Roxane and Bergerac, France.
+      <div id="catalog-title">"THe Three Billy Goats Gruff" (2004)</div>
+             <div id="title-description">For string quintet and narrator. Commissioned by Cactus Pear Music Festival. Translated to Flemish for performance at Conservatory Ann Zee, Ostende, Belgium 2011</div>        
+      <div id="catalog-title">"To Be a Puddle" (2001)</div>
+             <div id="title-description">For cello and piano. Written for Judith Vander Weg and Christine Debus.</div>
+      <div id="catalog-title">"Out From Behind This Mask" (2000)</div>
+             <div id="title-description">For baritone voice and string quartet. Commissioned by Timothy Jones and The Cactus Pear Music Festival, Stephanie Sant'Ambrogio, Director.</div>
+      <div id="catalog-title">"The Tempest" (1999)</div>
+             <div id="title-description">Song cycle for soprano, flute, clarinet, cello and piano. Commissioned by St. Mary's Hall Children's choir, Douglas Boyer, Director</div>
+      <div id="catalog-title">"Serenata" (1996)</div>
+            <div id="title-description">For flute, English horn, and guitar. Commissioned by the Nova Trio.</div>
+      <div id="catalog-title">"Five Spirituals" (1995)</div>
+            <div id="title-description">For baritone voice and cellow. Written for Timothy Jones and Judith Vander Weg.</div>
+      <div id="catalog-title">"Prayer and Alleluia" (1995)</div>
+            <div id="title-description">For trumpet and piano. Written for David Balentine and Laura Wilson.</div>
+      <div id="catalog-title">"A Wiser Man" (1995)</div>
+            <div id="title-description">For cello and piano. Commissioned by Kathryn Mason Hodges in honor of the 50th birthday of Donald Hodges.</div>
+      <div id="catalog-title">"Daughter's Dance" (1994)</div>
+            <div id="title-description">For string trio. Commissioned by Bruce Balentine for his daughter Bethanna's 16th birthday.</div>
+      <div id="catalog-title">"Fantasy After Eric Dolphy" (1992)</div>
+            <div id="title-description">For soprano saxaphone, piano, bass and drums. Written for Morgan King.</div>
+      <div id="catalog-title">"Rhapsody on The Simple Things in Life" (1993)</div>
+            <div id="title-description">For violin, trumpet, piano, bass and drums. Commissioned by Joan Christenson, Musical Offerings.</div>
+      <div id="catalog-title">"Frog and the Golden Ball" (1993)</div>
+            <div id="title-description">For woodwind, quintent. Commissioned by The King William Winds through a grant from the San Antonio Department of Arts and Cultural Affairs.</div>
+      <div id="catalog-title">"Postcard Songs" (1994)</div>
+            <div id="title-description">For medium-low voice and piano. Text by Deand Curtis.</div>
+      <div id="catalog-title">"Variations on Truth" (1976)</div>
+            <div id="title-description">For electronic tape</div>
+      <div id="catalog-title">"Eclant" (1975)</div>
+            <div id="title-description">For horn, percussion and tape. Performed at the Electronic Music Plus Festival, West Virginia and at the Royal Academy of Music London for David Sternbach on horn.</div>
+      <div id="catalog-title">"Full Frontal Lobes" (2010)</div>
+            <div id="title-description">Commissioned by Musical Offerings for Jazz Meets Classical Series. Premiered at the Instituto de Mexico in San Antonio, Texas, April 2010. Duration: 15 minutes</div>
+   </div>
+   <div id="catalog-category"><i>Incidental Music and Theater</i>
+      <div id="catalog-title">"Pinochio">
+            <div id="title-description">Incidental music and orchestrations for original musical by Douglas Balentine Summer, 1994. Production at the Point Theater, Ingram, Texas. Commissioned by the Hill Country Arts Foundation.</div>
+      <div id="catalog-title">"The Tempest" (1983)</div>
+            <div id="title-description">Seven songs and incidental music. Commissioned for the production of Shakespeare's "The Tempest".</div>
+      <div id="catalog-title">"Beowulf" (1978)</div>
+            <div id="title-description">Electronic tape score. Commissioned for a KUT-FM Austin radio production of Beowulf.</div>
   </div>
+
+  </div> 
   </div>     
   </div>

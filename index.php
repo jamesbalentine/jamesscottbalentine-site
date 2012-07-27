9c34f0c60da935e9a953c7bd6da61f9bb65caaca
BenBalentine <myrcutio@gmail.com>
2012-07-27 11:55:29 -0500

    I dunno what he did. I'm just the messenger.

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 		</script>
 	</head>
 	<body>
-		<div style="position: fixed; bottom: 0; left: 200px; z-index: 1;">
+		<div style="position: fixed; bottom: 0; left: 0; z-index: 1; padding: 10px 0 10px 0; margin: 0;">
 			<?php include "inc/audioplayer.html"; ?>
 		</div>
 		<iframe id="inner" name="inner" src="inc/innerframe.php" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></iframe>

d4b6f688fd3c9a29aa2155b1cc3afdd846c9b5cf
James Balentine <guildhian@sbcglobal.net>
2012-08-04 17:57:34 -0500

    updated styles on catalog page

--- inc/catalog.php
+++ inc/catalog.php
@@ -5,32 +5,40 @@
     $doc->load( 'catalog.xml' );
 
     $sections = $doc->getElementsByTagName( "section" );
-    foreach( $sections as $section ) {
+    echo "<div class='catalog-section-title-menulist'>";
+        foreach( $sections as $key=>$section ) {
+            $sectionTitle = filter_var($section->getAttribute('category'), FILTER_SANITIZE_STRING);
+            echo "<div class='catalog-section-title-menu'><a href='#$key'>$sectionTitle</a></div>";
+        }
+    echo "</div>";
+    foreach( $sections as $key=>$section ) {
         $sectionTitle = filter_var($section->getAttribute('category'), FILTER_SANITIZE_STRING);
         echo "
-        <div class='catalog-section'><span class='catalog-section-title'>$sectionTitle</span>
+        <span class='catalog-section-title'><a id='$key'>$sectionTitle</a></span>
+        <div class='catalog-section'>
         ";
         $compositions = $section->getElementsByTagName( "composition" );
         foreach( $compositions as $composition ) {
 
-                $name = filter_var($composition->getElementsByTagName("name")->item(0)->nodeValue, FILTER_SANITIZE_STRING);
-                $description = filter_var($composition->getElementsByTagName("description")->item(0)->nodeValue, FILTER_SANITIZE_STRING);
-                $audiourl = filter_var($composition->getElementsByTagName("audiourl")->item(0)->getAttribute('value'), FILTER_SANITIZE_STRING);
-                $score = filter_var($composition->getElementsByTagName("score")->item(0)->getAttribute('value'), FILTER_SANITIZE_STRING);
-
-                echo "
-                <div class='catalog-composition'>
-                	<div class='catalog-composition-title'>$name</div>
-                	<div class='catalog-composition-description'>$description</div>
+            $name = filter_var($composition->getElementsByTagName("name")->item(0)->nodeValue, FILTER_SANITIZE_STRING);
+            $description = filter_var($composition->getElementsByTagName("description")->item(0)->nodeValue, FILTER_SANITIZE_STRING);
+            $audiourl = filter_var($composition->getElementsByTagName("audiourl")->item(0)->getAttribute('value'), FILTER_SANITIZE_STRING);
+            $score = filter_var($composition->getElementsByTagName("score")->item(0)->getAttribute('value'), FILTER_SANITIZE_STRING);
+            $price = filter_var($composition->getElementsByTagName("price")->item(0)->nodeValue, FILTER_SANITIZE_STRING);
+            
+            echo "
+            <div class='catalog-composition'>
+            	<div class='catalog-composition-title'>$name</div>
+            	<div class='catalog-composition-description'>$description</div>
+                <div class='catalog-composition-links'>
         			<div class='audiourl'><a href='$audiourl'>$name sample</a></div>
                     <div class='score'><a href='$score'>$name score</a></div>
+                    <div class='price'>$price</div>
                 </div>
-                ";
-
+            </div>
+            ";
         }
-        echo "
-        </div>
-        ";
+        echo "</div>";
     }
 ?>
 </div>
--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 		</script>
 	</head>
 	<body style="overflow: hidden;">
-		<div style="position: fixed; bottom: 0; left: 22.4%; z-index: 1; padding: 10px 0 10px 0; margin: 0;">
+		<div style="position: fixed; bottom: 0; z-index: 1; padding: 10px 0 10px 0; margin: 0; background-color: rgba(0, 0, 0, 0.8); width: 100%; height: 70px;">
 			<?php include "inc/audioplayer.html"; ?>
 		</div>
 		<iframe id="inner" name="inner" src="inc/innerframe.php" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></iframe>

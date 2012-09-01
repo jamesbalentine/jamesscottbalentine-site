37367a9f2f253e5fad05927523b8b3415be76776
Ben Balentine <ben.balentine@bbalentine-mbpro.satx.rr.com>
2012-09-01 18:55:39 -0500

    added buffer to inner iframe

--- index.php
+++ index.php
@@ -4,14 +4,13 @@
 <html>
 	<head>
 		<link rel="stylesheet" type="text/css" href="css/player.css">
-		<script type="text/javascript">
-			window.onbeforeunload = null;
-		</script>
 	</head>
 	<body style="overflow: hidden;">
-		<div style="position: fixed; bottom: 0; z-index: 1; padding: 10px 0 10px 0; margin: 0; background-color: rgba(0, 0, 0, 0.8); width: 100%; height: 70px;">
+		<div style="position: fixed; bottom: 0; left: 0; right: 0; z-index: 1; padding: 10px 0 10px 0; margin: 0; background-color: rgba(0, 0, 0, 0.8); width: 100%; height: 70px;">
 			<?php include "inc/audioplayer.html"; ?>
 		</div>
-		<iframe id="inner" name="inner" src="inc/innerframe.php" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></iframe>
+		<div id="innerframe-wrapper" style="position: absolute; top: 0; bottom: 90px; left: 0; right: 0;">
+		<iframe id="inner" name="inner" src="inc/innerframe.php" style="position: relative; left: 0; right: 0; width: 100%; height: 100%;"></iframe>
+		</div>
 	</body>
 </html>
